f83443da3a64cb46b2e1296ebeac861138957203
Ekam Nijjar <125761106+ekam-n@users.noreply.github.com>
2025-01-26 10:55:06 -0800

    changes to hero.tsx

--- app/hero.tsx
+++ app/hero.tsx
@@ -3,11 +3,13 @@ export default function Hero() {
     <div className="bg-black flex flex-col items-center min-h-screen">
       {/* Container with full-width rounded box */}
       <div className="bg-[#001044] rounded-lg shadow-lg p-8 px-12 text-white w-full mx-4 mt-4">
-        <h1 className="text-4xl mb-6 text-left">
-          I create <span className="text-blue-400">videogame</span> design & development
+        <h1 className="text-4xl font-semibold mb-6 text-left">
+          I love to <span className="text-blue-400">design & develop</span>
+          <br />
+          <span className="font-semibold">experiences and applications</span>
         </h1>
         <p className="text-lg text-left">
-          I'm Ekam, a creator of immersive and innovative videogame experiences.
+          Especially videogames!
         </p>
         <div className="flex items-center justify-start mt-6">
           <div className="flex items-center gap-2">
